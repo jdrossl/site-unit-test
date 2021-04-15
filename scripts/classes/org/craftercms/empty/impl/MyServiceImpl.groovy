aa81985be75ed72e7fe891794e7a6d53069e7b54
admin admin <evaladmin@example.com>
2021-04-15 14:04:07 -0600

    User admin wrote content /scripts/classes/org/craftercms/empty/impl/MyServiceImpl.groovy

---
+++ scripts/classes/org/craftercms/empty/impl/MyServiceImpl.groovy
@@ -0,0 +1,11 @@
+package org.craftercms.emtpy.impl
+
+import org.craftercms.empty.api.MyService
+
+class MyServiceImpl implements MyService {
+    
+    def getMessage() {
+        "Hello World!"
+    }
+    
+}
